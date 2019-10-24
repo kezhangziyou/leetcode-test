8d39dfc27572bc834978dbcab98152935968edb6
ZhangKe <zhangke306shdx@163.com>
2019-10-24 16:05:46 +0800

    新增 problem2:两数想加

---
+++ src/main/java/problem2/ListNode.java
@@ -0,0 +1,15 @@
+package problem2;
+
+/**
+ * @author zhangke
+ * @version 1.0
+ * @className ListNode
+ * @description
+ * @date 2019/10/24 3:05 PM
+ **/
+
+public class ListNode {
+	int val;
+	ListNode next;
+	ListNode(int x) { val = x; }
+}
